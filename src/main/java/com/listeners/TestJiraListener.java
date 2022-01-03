359d89730247f83d66bba99dd85c199aea4e1c83
SayakGithub01 <sayak.majumdermca@gmail.com>
2022-01-03 19:06:20 +0530

    Update TestJiraListener.java

--- src/main/java/com/listeners/TestJiraListener.java
+++ src/main/java/com/listeners/TestJiraListener.java
@@ -30,8 +30,8 @@ public class TestJiraListener implements ITestListener {
 		if (isTicketReady) {
 			// raise jira ticket:
 			System.out.println("is ticket ready for JIRA: " + isTicketReady);
-			JiraServiceProvider jiraSp = new JiraServiceProvider("https://naveenautomationlabs.atlassian.net",
-					"naveenanimation20@gmail.com", "jaizMlfaUkTfffNOVPq29B29", "TA");
+			JiraServiceProvider jiraSp = new JiraServiceProvider("https://rajeshprasad.atlassian.net",
+					"rprprprprprp977@gmail.com", "FM7I61AsqAjcBlkjHGO9BB2E", "BUG");
 			String issueSummary = result.getMethod().getConstructorOrMethod().getMethod().getName()
 					+ "got failed due to some assertion or exception";
 			String issueDescription = result.getThrowable().getMessage() + "\n";
